63f0cbe6229d986ca00e3c65ea2cb55795f2847f
ascend2001 <avnishsengupta1@g.ucla.edu>
2022-02-16 20:14:00 -0800

    Added in About Us Landing Graphic-not responsive

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import AboutUsLandingGraphic from '../assets/AboutUsLandingGraphic.svg';
+
 function AboutUsPage(){
     return(
-        <div>
+        <div className="flex flex-col w-full">
             <Header />
-            <p>This is the About Us Page.</p>
+            <div>
+                <h1 className="text-dark-blue font-semibold sm:my-16 text-center xl:text-left tracking-wide xl:absolute xl:left-24 xl:top-72 text-3xl xl:text-6xl">{'{'}About Us{'}'}</h1>
+                <h1 className="text-header-orange font-semibold text-center tracking-wide xl:absolute xl:right-24 xl:bottom-0 text-3xl xl:text-4xl AboutUsText">Cultivating a thriving community of data science professionals at UCLA</h1>
+                <img src={AboutUsLandingGraphic} alt="Graphs"/>
+            </div>
             <Footer />
         </div>
     )
